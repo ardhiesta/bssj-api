c0fbb78fd5c827d4fa6af456f90080c00ab44576
e9739b <pevantiavania@gmail.com>
2022-12-08 10:48:35 +0700

    menambahkan validasi

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -13,6 +13,25 @@ class DepositController extends Controller{
         $deposit->user_id = $request->user_id;
         $deposit->deposit_weight = $request->deposit_weight; 
         $deposit->amount = $request->amount; 
+
+        $deposit_request = Validator::make($request->all(), [
+            "user_id" => "required",
+            "deposit_weight" => "required",
+            "amount" => "required",
+        ], [
+            "required" => "Atleast one attribute should be edited!"
+        ]);
+
+        if ($deposit_request->fails()) {
+            return $this->sendError($deposit_request->errors(), "Unprocessable content", 422);
+        }
+
+        $validated = $deposit_request->validated();
+
+        foreach ($validated as $key => $value) {
+            $deposit->$key = $value;
+        }
+
         $deposit->save();
  
         return $this->sendResponse([
